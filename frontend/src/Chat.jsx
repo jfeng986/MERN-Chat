7cec12b853b4f3ae658ed2982220a6d051fbdcbb
JunyiF <junyifeng98@gmail.com>
2023-03-29 16:24:20 -0700

    added avatar

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,4 +1,5 @@
 import { useEffect,useState } from "react"
+import Avatar from "./Avatar.jsx"
 
 export default function Chat() {
     const[ws,setWs] = useState(null);
@@ -22,26 +23,10 @@ export default function Chat() {
         if('online' in messageData){
             showOnlineUser(messageData.online);
         }
-        /*
-        event.data.text().then(messageString=>{
-            const message = JSON.parse(messageString);
-            console.log(message);
-        })
-        */
-        /*
-        event.preventDefault();
-        const message = event.target.elements.message.value;
-        event.target.elements.message.value = "";
-        ws.send(JSON.stringify({
-            type: "message",
-            message
-        }));
-        */
     }
-
     return(
         <div className="flex h-screen">
-            <div className="bg-green-100 w-1/3 pl-4 pt-4">
+            <div className="bg-green-100 w-1/4 pl-4 pt-4">
                 <div className="text-green-600 font-bold flex gap-2 mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64" />
@@ -50,11 +35,12 @@ export default function Chat() {
                 </div>
                 {Object.keys(onlineUser).map(userId=>
                 <div key={userId} 
-                className="border-b border-gray-100 py-2">
-                    {onlineUser[userId]}
+                className="border-b border-gray-100 p-2 flex items-center gap-2">
+                <Avatar username={onlineUser[userId]} userId={userId} online={onlineUser}/>
+                <span className="text-xl">{onlineUser[userId]}</span>
                 </div>)}
             </div>
-            <div className="bg-green-300 w-2/3 p-1 flex flex-col">
+            <div className="bg-green-300 w-3/4 p-1 flex flex-col">
                 <div className="flex-grow">messages</div>
                 <div className="flex gap-2">
                     <input type="text" 
