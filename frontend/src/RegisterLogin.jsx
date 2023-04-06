911c65b887b72cc633f3f7041bc5b052f17727cb
JunyiF <junyifeng98@gmail.com>
2023-04-06 15:17:05 -0700

    set login as default

--- frontend/src/RegisterLogin.jsx
+++ frontend/src/RegisterLogin.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoginOrRegister, setIsLoginOrRegister] = useState("register");
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
 
   async function handleSubmit(event) {
@@ -17,50 +17,63 @@ export default function Register() {
   }
 
   return (
-    <div className="bg-green-50 h-screen flex items-center">
-      <form className="w-64 mx-auto" onSubmit={handleSubmit}>
-        <input
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          type="text"
-          placeholder="username"
-          className="block w-full rounded-sm p-2 mb-2"
-        />
-        <input
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          type="password"
-          placeholder="password"
-          className="block w-full rounded-sm p-2 mb-2"
-        />
-        <button className="bg-green-500 text-white w-full rounded-sm p-2">
-          {isLoginOrRegister === "register" ? "Register" : "Login"}
-        </button>
-        <div className="text-center mt-2">
-          {isLoginOrRegister === "register" && (
-            <div>
-              Already a member?
-              <button
-                onClick={() => setIsLoginOrRegister("login")}
-                className="ml-3"
-              >
-                Login here
-              </button>
-            </div>
-          )}
-          {isLoginOrRegister === "login" && (
-            <div>
-              Don't have an account?
-              <button
-                onClick={() => setIsLoginOrRegister("register")}
-                className="ml-1"
-              >
-                register here
-              </button>
-            </div>
-          )}
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="text-green-600 bg-green-50 font-bold flex gap-2 px-6 text-xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64"
+          />
+        </svg>
+        ChatRoom
+      </div>
+      <div className="bg-green-50 h-screen flex items-center">
+        <form className="w-64 mx-auto" onSubmit={handleSubmit}>
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            type="text"
+            placeholder="username"
+            className="block w-full rounded-sm p-2 mb-2"
+          />
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            placeholder="password"
+            className="block w-full rounded-sm p-2 mb-2"
+          />
+          <button className="bg-green-500 text-white w-full rounded-sm p-2">
+            {isLoginOrRegister === "register" ? "Register" : "Login"}
+          </button>
+          <div className="text-center mt-2">
+            {isLoginOrRegister === "login" && (
+              <div>
+                Don't have an account?
+                <button onClick={() => setIsLoginOrRegister("register")}>
+                  register here
+                </button>
+              </div>
+            )}
+            {isLoginOrRegister === "register" && (
+              <div>
+                Already a member?
+                <button onClick={() => setIsLoginOrRegister("login")}>
+                  Login here
+                </button>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
